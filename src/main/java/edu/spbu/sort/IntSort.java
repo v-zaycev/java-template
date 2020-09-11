01923e2bd4fd29b40826d2b7690c82442d55343b
v-zaycev <vozon.o@yandex.ru>
2020-09-11 22:13:50 +0300

    Added merge sort for integer array

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -7,12 +7,48 @@ import java.util.List;
 /**
  * Created by artemaliev on 07/09/15.
  */
-public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
+public class IntSort
+{
+  public static void merge (int[] main_array, int[] copy_array, int left, int right)
+  {
+    if (left<right)
+    {
+      int middle = (left + right) / 2;
+      merge(main_array, copy_array, left, middle);
+      merge(main_array, copy_array, middle+1, right);
+
+      int i = left, k = left, j = middle+1;
+      while (i <= middle && j <= right)
+        if (copy_array[i] < copy_array[j])
+          main_array[k++]=copy_array[i++];
+        else
+          main_array[k++]=copy_array[j++];
+      while (i<=middle)
+        main_array[k++]=copy_array[i++];
+      while (j<=right)
+        main_array[k++]=copy_array[j++];
+      for (int a=left;a<right+1;a++)
+      {
+        copy_array[a]=main_array[a];
+      }
+    }
+  }
+
+  public static void sort (int[] array)
+  {
+    int[] array_copy=array.clone();
+    merge(array,array_copy,0,array.length-1);
   }
 
   public static void sort (List<Integer> list) {
     Collections.sort(list);
   }
+
+/*  public static void main (String[] args)
+  {
+    int[] array={1,7,3,54,6,2,4,7,9,41,12, 57,11};
+    sort(array);
+    for (int j : array) System.out.print(j+" ");
+  }*/
 }
+
